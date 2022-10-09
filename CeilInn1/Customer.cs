ebc4c783586a3b0e07bcc1f9eab7ff1a41509b08
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-09 15:55:38 +0800

    feat: customer class

---
+++ CeilInn1/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CeilInn1
+{
+    [Serializable]
+    public class Customer
+    {
+        public string AccountNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmergencyName { get; set; }
+        public string EmergencyPhone { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Customer client = (Customer)obj;
+
+            if (client.AccountNumber.Equals(this.AccountNumber))
+                return true;
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
