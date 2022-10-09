795633a7043f13551f4358e06a48a0319e218fb7
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-09 19:20:02 +0800

    feat: add ceil in home form

---
+++ CeilInn1/CeilInn.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CeilInn1
+{
+    partial class CeilInn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOccupancies = new System.Windows.Forms.Button();
+            this.btnRooms = new System.Windows.Forms.Button();
+            this.btnEmployees = new System.Windows.Forms.Button();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnPayments = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Elephant", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(272, 233);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(246, 93);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnOccupancies
+            // 
+            this.btnOccupancies.Font = new System.Drawing.Font("Elephant", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOccupancies.Location = new System.Drawing.Point(272, 15);
+            this.btnOccupancies.Name = "btnOccupancies";
+            this.btnOccupancies.Size = new System.Drawing.Size(246, 93);
+            this.btnOccupancies.TabIndex = 8;
+            this.btnOccupancies.Text = "Occupancies...";
+            this.btnOccupancies.UseVisualStyleBackColor = true;
+            // 
+            // btnRooms
+            // 
+            this.btnRooms.Font = new System.Drawing.Font("Elephant", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRooms.Location = new System.Drawing.Point(12, 124);
+            this.btnRooms.Name = "btnRooms";
+            this.btnRooms.Size = new System.Drawing.Size(246, 93);
+            this.btnRooms.TabIndex = 7;
+            this.btnRooms.Text = "Rooms...";
+            this.btnRooms.UseVisualStyleBackColor = true;
+            // 
+            // btnEmployees
+            // 
+            this.btnEmployees.Font = new System.Drawing.Font("Elephant", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployees.Location = new System.Drawing.Point(12, 233);
+            this.btnEmployees.Name = "btnEmployees";
+            this.btnEmployees.Size = new System.Drawing.Size(246, 93);
+            this.btnEmployees.TabIndex = 6;
+            this.btnEmployees.Text = "Employees...";
+            this.btnEmployees.UseVisualStyleBackColor = true;
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Font = new System.Drawing.Font("Elephant", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCustomers.Location = new System.Drawing.Point(12, 15);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(246, 93);
+            this.btnCustomers.TabIndex = 5;
+            this.btnCustomers.Text = "Customers...";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            // 
+            // btnPayments
+            // 
+            this.btnPayments.Font = new System.Drawing.Font("Elephant", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPayments.Location = new System.Drawing.Point(272, 124);
+            this.btnPayments.Name = "btnPayments";
+            this.btnPayments.Size = new System.Drawing.Size(246, 93);
+            this.btnPayments.TabIndex = 10;
+            this.btnPayments.Text = "Payments...";
+            this.btnPayments.UseVisualStyleBackColor = true;
+            // 
+            // CeilInn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 343);
+            this.Controls.Add(this.btnPayments);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOccupancies);
+            this.Controls.Add(this.btnRooms);
+            this.Controls.Add(this.btnEmployees);
+            this.Controls.Add(this.btnCustomers);
+            this.MaximizeBox = false;
+            this.Name = "CeilInn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ceil Inn";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOccupancies;
+        private System.Windows.Forms.Button btnRooms;
+        private System.Windows.Forms.Button btnEmployees;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnPayments;
+    }
+}
+
