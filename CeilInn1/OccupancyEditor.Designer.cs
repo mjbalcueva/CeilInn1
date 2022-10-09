41463cf8e9a825dafc807b45a6fb0d4a7577a90c
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-09 16:24:20 +0800

    feat: add occupancy editor forms

---
+++ CeilInn1/OccupancyEditor.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace CeilInn1
+{
+    partial class OccupancyEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtPhoneUse = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtRateApplied = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtRoomDescription = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpDateOccupied = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmployeeNumber = new System.Windows.Forms.TextBox();
+            this.txtAccountNumber = new System.Windows.Forms.TextBox();
+            this.txtOccupancyNumber = new System.Windows.Forms.TextBox();
+            this.txtRoomNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(11, 241);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(307, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "__________________________________________________";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(245, 267);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(66, 23);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOK.Location = new System.Drawing.Point(173, 267);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(65, 23);
+            this.btnOK.TabIndex = 16;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // txtPhoneUse
+            // 
+            this.txtPhoneUse.Location = new System.Drawing.Point(249, 218);
+            this.txtPhoneUse.Name = "txtPhoneUse";
+            this.txtPhoneUse.Size = new System.Drawing.Size(62, 20);
+            this.txtPhoneUse.TabIndex = 14;
+            this.txtPhoneUse.Text = "0.00";
+            this.txtPhoneUse.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(180, 221);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(63, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Phone Use:";
+            // 
+            // txtRateApplied
+            // 
+            this.txtRateApplied.Location = new System.Drawing.Point(101, 218);
+            this.txtRateApplied.Name = "txtRateApplied";
+            this.txtRateApplied.Size = new System.Drawing.Size(63, 20);
+            this.txtRateApplied.TabIndex = 12;
+            this.txtRateApplied.Text = "0.00";
+            this.txtRateApplied.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(11, 221);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 13);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Rate Applied:";
+            // 
+            // txtRoomDescription
+            // 
+            this.txtRoomDescription.Location = new System.Drawing.Point(14, 186);
+            this.txtRoomDescription.Name = "txtRoomDescription";
+            this.txtRoomDescription.Size = new System.Drawing.Size(297, 20);
+            this.txtRoomDescription.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 162);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Room Number:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(304, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Room Rented ______________________________________";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(170, 112);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(141, 20);
+            this.txtCustomerName.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Customer #:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(303, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Processed for ______________________________________";
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(170, 63);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(141, 20);
+            this.txtEmployeeName.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Employee #:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(302, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Processed by ______________________________________";
+            // 
+            // dtpDateOccupied
+            // 
+            this.dtpDateOccupied.Location = new System.Drawing.Point(101, 13);
+            this.dtpDateOccupied.Name = "dtpDateOccupied";
+            this.dtpDateOccupied.Size = new System.Drawing.Size(210, 20);
+            this.dtpDateOccupied.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Date Occupied:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Occupancy #:";
+            // 
+            // txtEmployeeNumber
+            // 
+            this.txtEmployeeNumber.Location = new System.Drawing.Point(101, 63);
+            this.txtEmployeeNumber.Name = "txtEmployeeNumber";
+            this.txtEmployeeNumber.Size = new System.Drawing.Size(63, 20);
+            this.txtEmployeeNumber.TabIndex = 4;
+            // 
+            // txtAccountNumber
+            // 
+            this.txtAccountNumber.Location = new System.Drawing.Point(101, 112);
+            this.txtAccountNumber.Name = "txtAccountNumber";
+            this.txtAccountNumber.Size = new System.Drawing.Size(63, 20);
+            this.txtAccountNumber.TabIndex = 7;
+            // 
+            // txtOccupancyNumber
+            // 
+            this.txtOccupancyNumber.Location = new System.Drawing.Point(101, 269);
+            this.txtOccupancyNumber.Name = "txtOccupancyNumber";
+            this.txtOccupancyNumber.Size = new System.Drawing.Size(61, 20);
+            this.txtOccupancyNumber.TabIndex = 23;
+            // 
+            // txtRoomNumber
+            // 
+            this.txtRoomNumber.Location = new System.Drawing.Point(101, 159);
+            this.txtRoomNumber.Name = "txtRoomNumber";
+            this.txtRoomNumber.Size = new System.Drawing.Size(63, 20);
+            this.txtRoomNumber.TabIndex = 10;
+            // 
+            // OccupancyEditor
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(326, 307);
+            this.Controls.Add(this.txtRoomNumber);
+            this.Controls.Add(this.txtOccupancyNumber);
+            this.Controls.Add(this.txtAccountNumber);
+            this.Controls.Add(this.txtEmployeeNumber);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPhoneUse);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtRateApplied);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtRoomDescription);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpDateOccupied);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "OccupancyEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ceil Inn - Occupancy Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        public System.Windows.Forms.TextBox txtPhoneUse;
+        private System.Windows.Forms.Label label10;
+        public System.Windows.Forms.TextBox txtRateApplied;
+        private System.Windows.Forms.Label label9;
+        public System.Windows.Forms.TextBox txtRoomDescription;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        public System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.DateTimePicker dtpDateOccupied;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtEmployeeNumber;
+        public System.Windows.Forms.TextBox txtAccountNumber;
+        public System.Windows.Forms.TextBox txtOccupancyNumber;
+        public System.Windows.Forms.TextBox txtRoomNumber;
+    }
+}
