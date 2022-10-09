ed33616edab517aef0d211ffaaa584b99348aa21
mj balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-10-09 17:52:34 +0800

    feat: add payment editor calculate button functionality

--- CeilInn1/PaymentEditor.cs
+++ CeilInn1/PaymentEditor.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CeilInn1
@@ -15,6 +8,17 @@ namespace CeilInn1
         public PaymentEditor()
         {
             InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // If there is no payment amount, don't do anything
+            if (string.IsNullOrEmpty(txtAmountCharged.Text))
+                return;
+
+            txtSubTotal.Text = ((Convert.ToInt16(txtTotalNights.Text) * Convert.ToDouble(txtAmountCharged.Text)) + Convert.ToDouble(txtPhoneUse.Text)).ToString("F");
+            txtTaxAmount.Text = (Convert.ToDouble(txtSubTotal.Text) * Convert.ToDouble(txtTaxRate.Text) / 100).ToString("F");
+            txtTotalAmountPaid.Text = (Convert.ToDouble(txtSubTotal.Text) + Convert.ToDouble(txtTaxAmount.Text)).ToString("F");
         }
     }
 }
